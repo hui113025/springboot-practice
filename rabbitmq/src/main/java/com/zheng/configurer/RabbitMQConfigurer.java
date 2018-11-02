79a0e427984da110434bd2c298043b8156d3fd55
hui113025 <zhenghui@duia.com>
2018-11-02 17:20:28 +0800

    RabbitMQ添加手动接收模式

--- rabbitmq/src/main/java/com/zheng/configurer/RabbitMQConfigurer.java
+++ rabbitmq/src/main/java/com/zheng/configurer/RabbitMQConfigurer.java
@@ -1,11 +1,17 @@
 package com.zheng.configurer;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- *
  * @author zhenghui
  * @date 2018/4/11
  * 描述：配置队列
@@ -14,6 +20,30 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class RabbitMQConfigurer {
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+    @Value("${spring.rabbitmq.username}")
+    private String userName;
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host, port);
+        cachingConnectionFactory.setUsername(userName);
+        cachingConnectionFactory.setPassword(password);
+        cachingConnectionFactory.setVirtualHost("/");
+        cachingConnectionFactory.setPublisherConfirms(true);
+        return cachingConnectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        return rabbitTemplate;
+    }
 
     //取代directQueue
     @Bean
@@ -49,14 +79,14 @@ public class RabbitMQConfigurer {
 
     //直连交换机：完全匹配
     @Bean
-    public DirectExchange directExchange(){
+    public DirectExchange directExchange() {
 
         return new DirectExchange("directExchange");
     }
 
     //主题交换机：通配符匹配
     @Bean
-    public TopicExchange topicExchange(){
+    public TopicExchange topicExchange() {
         return new TopicExchange("topicExchange");
     }
 
@@ -68,19 +98,46 @@ public class RabbitMQConfigurer {
 
     //绑定直连交换机 一般用queue()替代
     @Bean
-    public Binding bindingDirectExchange(Queue directQueue, DirectExchange directExchange){
+    public Binding bindingDirectExchange(Queue directQueue, DirectExchange directExchange) {
         return BindingBuilder.bind(directQueue).to(directExchange).with("direct");
     }
 
     //绑定主题交换机
     @Bean
-    public Binding bindingTopicExchange(Queue topicQueue, TopicExchange topicExchange){
+    public Binding bindingTopicExchange(Queue topicQueue, TopicExchange topicExchange) {
         return BindingBuilder.bind(topicQueue).to(topicExchange).with("topic.message");
     }
 
     //绑定广播交换机
     @Bean
-    public Binding bindingFanoutExchange(Queue fanoutQueue, FanoutExchange fanoutExchange){
+    public Binding bindingFanoutExchange(Queue fanoutQueue, FanoutExchange fanoutExchange) {
         return BindingBuilder.bind(fanoutQueue).to(fanoutExchange);
+    }
+
+
+    /**
+     * 接受消息的监听，这个监听会接受消息队列的消息
+     * 针对消费者配置
+     *
+     * @return
+     */
+    @Bean
+    @Autowired
+    public SimpleMessageListenerContainer messageContainer(ConnectionFactory connectionFactory) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory());
+        container.setQueues(queue());
+        container.setExposeListenerChannel(true);
+        container.setMaxConcurrentConsumers(1);
+        container.setConcurrentConsumers(1);
+        container.setAcknowledgeMode(AcknowledgeMode.MANUAL); //设置确认模式手工确认
+        container.setMessageListener(new ChannelAwareMessageListener() {
+            @Override
+            public void onMessage(Message message, com.rabbitmq.client.Channel channel) throws Exception {
+                byte[] body = message.getBody();
+                System.out.println("接收者 HelloReceiver,手动收到消息 : " + new String(body));
+                channel.basicAck(message.getMessageProperties().getDeliveryTag(), false); //确认消息成功消费
+            }
+        });
+        return container;
     }
 }
